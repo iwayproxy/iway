19295235e8dc6745374f857368ab280e2635a56c
iway-proxy <iwayproxy@gmail.com>
2026-01-07 01:30:16 +0800

    remove dns cache, a new one will be

--- src/protocol/tuic/dns_cache.rs
+++
@@ -1,7 +0,0 @@
-// DNS cache removed. Previously implemented with `moka` but the feature
-// was deleted to reduce complexity. This file intentionally left minimal
-// to avoid build issues if referenced accidentally.
-
-// If you want to fully remove this file, delete it and remove any
-// remaining references. Currently the module declaration in
-// `src/protocol/tuic/mod.rs` has been removed.
