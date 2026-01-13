52dc6c4c36024a38df13831c53fa7ffd01e61440
iway-proxy <iwayproxy@gmail.com>
2026-01-14 01:07:19 +0800

    some improvements

--- src/net/util.rs
+++ src/net/util.rs
@@ -19,5 +19,27 @@ static LOCAL_IPS: Lazy<HashSet<IpAddr>> = Lazy::new(|| {
 });
 
 pub fn is_local_addr(addr: &SocketAddr) -> bool {
-    LOCAL_IPS.contains(&addr.ip())
+    let ip = addr.ip();
+
+    // Quick accept for loopback addresses
+    if ip.is_loopback() {
+        return true;
+    }
+
+    match ip {
+        IpAddr::V4(_) => LOCAL_IPS.contains(&ip),
+        IpAddr::V6(v6) => {
+            // Direct IPv6 match
+            if LOCAL_IPS.contains(&IpAddr::V6(v6)) {
+                return true;
+            }
+
+            // If this is an IPv4-mapped IPv6 address, check the mapped IPv4
+            if let Some(v4) = v6.to_ipv4() {
+                return LOCAL_IPS.contains(&IpAddr::V4(v4));
+            }
+
+            false
+        }
+    }
 }
